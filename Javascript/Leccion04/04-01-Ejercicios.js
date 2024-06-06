f2932e6198b86dd3631705ad48fefb5b99390027
pablocorrea <pablocorrea34@gmail.com>
2024-06-06 09:49:57 -0300

    agrego ejercicios js

--- Javascript/Leccion04/04-01-Ejercicios.js
+++ Javascript/Leccion04/04-01-Ejercicios.js
@@ -8,3 +8,21 @@ function saludar() {
 }
 saludar();
 console.log(nombre);
+
+
+function saludar2() {
+    let nombre2 = 'Raul';
+    console.log(nombre2);
+}
+console.log(nombre2);
+
+if(true) {
+    let edad2 = 33;
+    console.log(edad2);
+}
+console.log(edad2);
+
+const fechaNacimiento = 2006;
+console.log(fechaNacimiento);
+fechaNacimiento = 2003;
+console.log(fechaNacimiento);
