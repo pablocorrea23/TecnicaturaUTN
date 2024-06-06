1433d0348a4faf8d2e2407693b020eea55200772
pablocorrea <pablocorrea34@gmail.com>
2024-06-06 11:04:29 -0300

    agrego ejercicios

--- Javascript/Leccion03/03-01-OperadoresJavascript.js
+++ Javascript/Leccion03/03-01-OperadoresJavascript.js
@@ -27,3 +27,47 @@ else{
     console.log("Fuera del rango");
 }
 
+descanso = true
+vacaciones = false
+
+if (descanso || vacaciones){
+    console.log("El trabajador puede ir al juego de su hijo");
+
+}else{
+    console.log("El trabajador no puede ir al juego de su hijo");
+}
+
+//operador ternario
+resultado = (3 > 2) ? "verdadero" : "falso";
+console.log(resultado);
+
+num = 6;
+resultado2 = (num % 2 == 0) ? 'Es numero PAR' : 'Es numero IMPAR';
+console.log(resultado2);
+
+//convertir string a number
+let miNumero = "10"; // esto es una cadena
+console.log(typeof miNumero);
+
+let edad2 = Number(miNumero); //esta es la funcion que va a hacer la conversion
+console.log(edad2);
+
+
+if(edad2 >= 18){
+    console.log("Puede votar");
+}else{
+    console.log("No puede votar");
+}
+
+//haciendolo en ternario
+votacion = (edad2 >= 18) ? 'Puede votar' : 'No puede votar';
+console.log(votacion);
+
+//funcion isNan
+if(isNaN(edad2)){ //is not a number = no es un numero (devulve un boleano)
+    console.log("No es un numero");
+}else{
+    console.log("Es un numero");
+}
+
+
